00da5a45aa96b82d0e7a7bde2d41518538e3d01b
Tim Pinkerton <timpinkerton@mail.com>
2017-05-17 20:43:30 -0400

    adding profile.js

---
+++ profile.js
@@ -0,0 +1,52 @@
+
+//Require https module
+const https = require('https');
+//Require module for status codes
+const http = require('http');
+
+//Print error messages
+function printError(error) {
+  console.error(error.message);
+}
+
+//Funcion to print message to the console
+function printMessage(username, badgeCount, points) {
+  const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+  console.log(message);
+}
+
+function get(username) {
+  try {
+// Connect to the API URL (https://teamtreehouse.com/username.json)
+const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+      if (response.statusCode === 200) {    
+        let body = "";
+        // Read the data
+        response.on('data', data => {
+          body += data.toString();            
+        });
+
+        response.on('end', () => {
+          try {
+          // Parse the data  
+            const profile = JSON.parse(body);
+          // Print the data
+            printMessage(username, profile.badges.length, profile.points.JavaScript);
+          } catch (error) {
+            printError(error);
+          }
+        });
+      } else {
+        const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
+        const statusCodeError = new Error(message); 
+        printError(statusCodeError);
+      }
+    });
+  
+  request.on('error', printError);
+  } catch (error) {
+    printError(error);
+  }
+}
+
+module.exports.get = get; 
